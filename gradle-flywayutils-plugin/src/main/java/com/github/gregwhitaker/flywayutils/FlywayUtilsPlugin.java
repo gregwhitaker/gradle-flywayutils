c5136197b51a7b143917146174ebce2cc69604ca
gregwhitaker <greg.whitaker@nike.com>
2021-04-27 15:13:25 -0700

    Initial Commit

---
+++ gradle-flywayutils-plugin/src/main/java/com/github/gregwhitaker/flywayutils/FlywayUtilsPlugin.java
@@ -0,0 +1,12 @@
+package com.github.gregwhitaker.flywayutils;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class FlywayUtilsPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+
+    }
+}
